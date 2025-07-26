c0afa4dc150fa66429835f57e591ee3f8878c993
ADARSH5141 <alphasingh81186@gmail.com>
2025-07-26 23:56:39 +0530

    new verison of yt reel

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
-let swiper = new Swiper(".slider", {
-  loop: true,
-  direction: 'vertical',
-  mousewheel: true,
-  spaceBetween: 5,
-  navigation:{
-    nextEl: ".down",
-    prevEl: ".up",
-  },
-  on: {
-    slideChange: function () {
-      // Pause all videos
-      document.querySelectorAll("video").forEach((video) => {
-        video.pause();
-        video.currentTime = 0;
-      });
+require("dotenv").config();;
+let express = require("express");
+let app = express();
+let {v4:uuidv4} = require("uuid");
+let mysql2 = require("mysql2");
 
-      // Wait a bit to ensure Swiper updates active index correctly
-      setTimeout(() => {
-        const activeSlide = document.querySelector(".swiper-slide-active");
-        const video = activeSlide.querySelector("video");
-        if (video) {
-          video.play().catch(err => console.log("Autoplay failed:", err));
+let path = require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.urlencoded({extended:true}));
+
+const connection = mysql2.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_ROOT,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
+})  
+app.get("/home",(req,res)=>{
+    let q = "select * from videos order by sortById desc";
+    connection.query(q,(err,result)=>{
+        if(err){
+            console.log(`err message from /home : ${err.message}`);
+           return; 
         }
-      }, 100);
-    }
-  }
-});
-
+        else{
+            res.render("index.ejs",{result})
+            // console.log(result);            
+        }
+    })
+})
+app.get("/newVideo",(req,res)=>{
+    res.render("addVideo.ejs")
+})
+app.post("/dedo",(req,res)=>{
+    let id = uuidv4();
+    let {link} = req.body;
+    let linkShort = link.replace("shorts","embed").split("").splice(0,41).join("");
+    // console.log(linkShort);
+    
+    let data  = [id,linkShort];
+    let q = `insert into videos (id,link) value (?)`;
+    connection.query(q,[data],(err,result)=>{
+        if(err){
+            console.log(`err found in /dedo : ${err.message}`);
+            return;
+        }
+        else{
+            console.log(result);
+            res.redirect("/home");
+        }
+    })
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`port ${process.env.PORT} is listening Continously`);
+    
+})
---
+++ public/used.js
@@ -0,0 +1,30 @@
+let swiper = new Swiper(".slider", {
+  loop: true,
+  direction: 'vertical',
+  mousewheel: true,
+  spaceBetween: 5,
+  navigation:{
+    nextEl: ".down",
+    prevEl: ".up",
+  },
+  on: {
+    slideChange: function () {
+      // Pause all videos
+      document.querySelectorAll("video").forEach((video) => {
+        video.pause();
+        video.currentTime = 0;
+      });
+
+      // Wait a bit to ensure Swiper updates active index correctly.
+
+      setTimeout(() => {
+        const activeSlide = document.querySelector(".swiper-slide-active");
+        const video = activeSlide.querySelector("video");
+        if (video) {
+          video.play().catch(err => console.log("Autoplay failed:", err));
+        }
+      }, 100);
+    }
+  }
+});
+
